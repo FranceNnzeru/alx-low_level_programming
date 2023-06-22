33b4989ce2eae48a5bafbef4426435dfa28e80cf
FranceNnzeru <Fnnzeru@gmail.com>
2023-06-21 17:30:45 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** print putchar without using printf
+/**
+ * main-print _putchar
  * use main.h
  * Return (0)
  */
@@ -17,5 +18,5 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 
-	return 0;
+	return (0);
 }
