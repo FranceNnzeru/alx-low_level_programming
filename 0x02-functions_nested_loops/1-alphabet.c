677e18f2cb17d995bc657d9f030b9428c02d229a
FranceNnzeru <Fnnzeru@gmail.com>
2023-06-21 18:12:23 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * print alphabet  - print all alphabet in lowecase
+ */
+
+void print_alphabet(void)
+{
+    char letter = 'a';
+
+    while (letter <= 'z')
+    {
+        _putchar(letter);
+        letter++;
+    }
+
+    _putchar('\n');
+}
+
