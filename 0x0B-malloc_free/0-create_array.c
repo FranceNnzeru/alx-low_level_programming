4e774a2a86ce150dda911dc5fd6ffe106eda7881
FranceNnzeru <Fnnzeru@gmail.com>
2023-07-12 15:09:26 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,8 +4,8 @@
 /**
  * create_array - create array of size and assign char c
  * @size: size of array
- * @c: character to assign
- * Description:creat array of size and assign char c
+ * @c:char to assign
+ * Description: create array of size and assign char c
  * Return: pointer to array, NULL if fail
  */
 char *create_array(unsigned int size, char c)
@@ -16,6 +16,9 @@ char *create_array(unsigned int size, char c)
 	str = malloc(sizeof(char) * size);
 
 	if (size == 0 || str == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
 		str[i] = c;
 	return (str);
 }
