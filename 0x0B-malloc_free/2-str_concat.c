1ab9b7c80a3c609d099bd32658c5d95c041225f9
FranceNnzeru <Fnnzeru@gmail.com>
2023-07-12 17:39:40 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -33,10 +33,10 @@ char *str_concat(char *s1, char *s2)
 
 				i = ci = 0;
 
-				while (si[i] != '\0')
+				while (s1[i] != '\0')
 				{
 					conct[i] = s1[i];
-					i++
+					i++;
 				}
 				while (s2[ci] != '\0')
 				{
