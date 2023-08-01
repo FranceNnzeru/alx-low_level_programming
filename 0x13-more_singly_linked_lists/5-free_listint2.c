b2d379bcc7d50083897180972d3fbac3a8dab90a
FranceNnzeru <Fnnzeru@gmail.com>
2023-07-31 17:44:08 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,7 +3,7 @@
 
 /**
  * free_listint2 - Frees a listint_t and sets the head to NULL.
- * @head: Doubel pointer to the head of the list.
+ * @head: Double  pointer to the head of the list.
  */
 void free_listint2(listint_t **head)
 {
